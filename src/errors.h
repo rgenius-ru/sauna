0233736ba129a04099f2e1a0eab9894520cfb72f
rg <rgenius@yandex.ru>
2022-12-15 12:52:45 +0300

    Add errors: heater, vapor

--- src/errors.h
+++ src/errors.h
@@ -7,9 +7,11 @@
 
 #define ERROR_TEMP_SENSOR_DISCONNECTED "Датчик температуры отключен" // "Error: Could not read temperature data"
 #define ERROR_TEMP_SENSOR_OVERHEAT "Перегрев воздуха" // "Error: Overheat temperature" 
+#define ERROR_HEATER "Отсутствует нагрев воздуха"
 
 #define ERROR_HUMIDITY_SENSOR_DISCONNECTED "Датчик влажности отключен" // "Failed to read from DHT sensor!"
 #define ERROR_HUMIDITY_SENSOR_OVERVAPOR "Переувлажнение воздуха"
+#define ERROR_VAPOR "Отсутствует увлажнение воздуха"
 
 struct Error {
   bool has_error;
